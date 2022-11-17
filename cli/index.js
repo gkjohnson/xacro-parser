899a5c1b1c28c4807e2a1ffe3d285d5a96fa9875
Garrett Johnson <garrett.kjohnson@gmail.com>
2022-11-17 18:35:07 +0900

    add cli updates

--- cli/index.js
+++ cli/index.js
@@ -1,23 +1,33 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const path = require('path');
-const { XacroParser } = require('../umd/index.cjs');
-const { JSDOM } = require('jsdom');
+
+import fs from 'fs';
+import path from 'path';
+import { XacroParser } from '../src/XacroParser.js';
+import { JSDOM } from 'jsdom';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+const argv = yargs(hideBin(process.argv))
+    .positional('input', {
+        type: 'string',
+    })
+    .option('oldorder', {
+        type: 'boolean',
+        default: false,
+    })
+    .option('package', {
+        type: 'string',
+        default: null,
+    })
+    .help()
+    .argv;
 
 const jsdom = new JSDOM();
 const window = jsdom.window;
 global.DOMParser = window.DOMParser;
 global.XMLSerializer = window.XMLSerializer;
 
-const args = process.argv;
-if (args.includes('--help')) {
-
-    console.log('xacro-parser <input-file> [--oldorder]');
-    process.exit(0);
-
-}
-
-const inputFile = args[2];
+let inputFile = argv._[0];
 if (!inputFile) {
 
     console.error('input file required');
@@ -25,12 +35,20 @@ if (!inputFile) {
 
 }
 
-const inOrder = !args.includes('--oldorder');
+inputFile = path.join(process.cwd(), inputFile);
+
+const inOrder = !argv.oldorder;
 const parser = new XacroParser();
 parser.inOrder = inOrder;
 parser.requirePrefix = inOrder;
 parser.localProperties = inOrder;
 parser.workingPath = path.dirname(inputFile);
+parser.rospackCommands.find = () => {
+
+    return path.join(process.cwd(), argv.package);
+
+};
+
 parser.getFileContents = p => {
 
     return fs.readFileSync(p, { encoding: 'utf8' });
