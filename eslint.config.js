3ce4e8e3b524e84ae1e1c47700f96d6652792e20
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-02-02 21:08:01 +0900

    Migrate to vite, vitest, and eslint

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+import js from '@eslint/js';
+import vitest from '@vitest/eslint-plugin';
+import globals from 'globals';
+import mdcs from 'eslint-config-mdcs';
+
+export default [
+    // files to ignore
+    {
+        name: 'files to ignore',
+        ignores: [
+            '**/node_modules/**',
+            '**/umd/**',
+        ],
+    },
+
+    // recommended
+    js.configs.recommended,
+
+    // base rules
+    {
+        name: 'base rules',
+        files: ['**/*.js'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            sourceType: 'module',
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },
+        rules: {
+            ...mdcs.rules,
+            'no-mixed-spaces-and-tabs': 'error',
+        },
+    },
+
+    // vitest
+    {
+        name: 'vitest rules',
+        files: ['**/*.test.js'],
+        plugins: {
+            vitest,
+        },
+        languageOptions: {
+            globals: {
+                ...vitest.environments.env.globals,
+            },
+        },
+        rules: {
+            ...vitest.configs.recommended.rules,
+            'vitest/valid-describe-callback': 0,
+        },
+    },
+];
